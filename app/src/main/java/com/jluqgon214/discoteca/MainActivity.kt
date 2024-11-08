e338085e23026c357224b4bb3ac5aec2645ae7dd
José Manuel Luque <jluqgon214@g.educaand.es>
2024-11-08 18:11:47 +0100

    Finaizado

--- app/src/main/java/com/jluqgon214/discoteca/MainActivity.kt
+++ app/src/main/java/com/jluqgon214/discoteca/MainActivity.kt
@@ -1,20 +1,33 @@
 package com.jluqgon214.discoteca
 
+import android.graphics.Color
+import android.graphics.Color.*
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val botonColor = findViewById<Button>(R.id.BotonColor)
+
+        botonColor.setOnClickListener {
+            val mainLayout = findViewById<ConstraintLayout>(R.id.main)
+            mainLayout.setBackgroundColor(rgb(Random.nextInt(256), Random.nextInt(256), Random.nextInt(256)))
         }
+
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
     }
 }
